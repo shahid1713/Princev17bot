29795ed82cdef09fa35ff53b1578fa7961a28f6c
shahid <Malik.shahid.1713@gmail.com>
2024-09-21 17:08:50 +0500

    V17bots

---
+++ settings.js
@@ -0,0 +1,20 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+
+global.owner = ['628xxx']
+global.botname = 'Alwaysaqioo'
+global.namaowner = 'Alwaysaqioo'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "Alwaysaqioo"
+global.sticker2 = "🌜"
+global.apikey = '_'
+global.capikey = '_'
+global.domain = '_'
+global.eggsnya = '_'
+global.location = '_'
+global.adana = '628XX'
+global.aqris = 'https://telegra.ph/file/1701509104e6531c8a4d1.jpg'
+global.aovo = '628XX'
+global.agopay = '628XX'
+global.mess = { 
+ wait: `wait...️`
+}
